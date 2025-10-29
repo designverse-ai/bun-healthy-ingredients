29eda6efb07b146596e3c5cb341880a53d4e1b8d
Alex Renoki <alex@renoki.org>
2025-10-29 07:42:15 +0200

    fix(ghsa): add 1000 packages limit per request using chunks

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,52 @@
 import { Octokit } from "@octokit/core"
+import type { Endpoints } from "@octokit/types"
 import { paginateRest } from "@octokit/plugin-paginate-rest"
 
 const PaginatedOctokit = Octokit.plugin(paginateRest)
+
+type GHSA = Endpoints["GET /advisories"]["response"]["data"][number]
 
 const octokit = new PaginatedOctokit({
   auth: Bun.env.GITHUB_TOKEN,
 })
 
 const fetchThreatFeed = async (packages: Bun.Security.Package[]) => {
-  return octokit.paginate("GET /advisories", {
-    ecosystem: "npm",
-    per_page: 100,
-    affects: packages.map((p) => [p.name, p.version].join("@")),
-    sort: "updated",
-    direction: "desc",
-  })
+  const feed: GHSA[] = []
+
+  // GHSA has a limit of 1000 packages per request, so we need to split the packages into multiple requests
+  const chunks = packages.reduce<Bun.Security.Package[][]>((chunks, pkg) => {
+    // If the first chunk is empty, add the package to it
+    if (chunks.length === 0) {
+      chunks.push([pkg])
+      return chunks
+    }
+
+    const lastChunk = chunks.at(-1)
+
+    // If the last chunk is not full, add the package to it
+    if (lastChunk && lastChunk.length < 1000) {
+      lastChunk.push(pkg)
+    } else {
+      // If the last chunk is full, create a new chunk
+      chunks.push([pkg])
+    }
+
+    return chunks
+  }, [])
+
+  for await (const chunk of chunks) {
+    feed.push(
+      ...(await octokit.paginate("GET /advisories", {
+        ecosystem: "npm",
+        per_page: 100,
+        affects: chunk.map((p) => [p.name, p.version].join("@")),
+        sort: "updated",
+        direction: "desc",
+      })),
+    )
+  }
+
+  return feed
 }
 
 export const scanner: Bun.Security.Scanner = {
